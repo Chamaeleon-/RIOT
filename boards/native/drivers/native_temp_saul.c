3fd8b294621df16c28c5b518de84b2639b5ef3ca
Chamaeleon <y0067315@tu-bs.de>
2020-03-18 17:05:59 +0100

    added ability to parse one csv line

--- boards/native/drivers/native_temp_saul.c
+++ boards/native/drivers/native_temp_saul.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "phydat.h"
 #include "board.h"
 #include "saul.h"
@@ -8,6 +9,8 @@
 #define MAXCHAR 1000
 
 FILE *sensorfile = NULL;
+int8_t last_value = 0;
+__time_t valid_until = 0;
 
 static int read(const void *dev, phydat_t *res) 
 {
@@ -17,16 +20,41 @@ static int read(const void *dev, phydat_t *res)
     res->scale = 0;
     res->unit = UNIT_TEMP_C;
     res->val[0] = 00;
-    if (sensorfile == NULL)
-    {
-        printf("Could not read file\n");
-        return 0;
-    }
-    while (fgets(str, MAXCHAR, sensorfile) != NULL)
-    {
-        res->val[0] = atoi(str);
-    }
-    return 1; //  number of values written into to result data structure [1-3] 
+
+    // if (now <= valid_until){
+    //     res->val[0] = last_value;
+    //     return 1;
+    // }
+    // else
+    // {
+        if (sensorfile == NULL)
+        {
+            printf("Could not read file\n");
+            return 0;
+        }
+        // while(valid_until < now){
+            if (fgets(str, MAXCHAR, sensorfile) != NULL)
+            {
+                char* value;
+                char* time;
+                time = strtok(str, ","); // maybe use strtok_r
+                value = strtok(NULL, ",");
+                last_value = atoi(value);
+                printf("Value valid until: %s\n", time);
+                // valid_until = &time;
+            }
+            else
+            {
+                return 0;
+            }
+            
+        // }
+        res->val[0] = last_value;
+        return 1; /* number of values written into to result data structure [1-3] */
+
+    // }
+    return 0;
+
 }
 
 const saul_driver_t native_temp_saul_driver = {
